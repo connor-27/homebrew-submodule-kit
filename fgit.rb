b060c26d3cecc876653282954bfde892d9d86eae
connor.27 <connor.27@kakaoent.com>
2024-06-17 08:55:14 +0900

    :sparkles:feature : brew 방식 변경
    feature/FANZ-162

--- fgit.rb
+++ fgit.rb
@@ -1,9 +1,12 @@
 class Fgit < Formula
   desc "Custom git aliases for submodule management"
   homepage "https://github.com/connor-27/homebrew-submodule-kit"
-  url "https://github.com/connor-27/homebrew-submodule-kit/raw/main/fgit.sh"
+  # url "https://github.com/connor-27/homebrew-submodule-kit/raw/main/fgit.sh"
   # shasum -a 256 fgit.sh
-  sha256 "1acbcf11aa4b0bb5ca0783d903512e406e129b2ffcd13acc38ba98ba97f772b2" # 쉘에 대한 해시값 넣기
+  # sha256 "86bf9c3c9b0ae31b0915d52a6d657ec40c3c32b074861b3af9c6f9df6f1fdb3c" # 쉘에 대한 해시값 넣기
+  # version "1.0.2"
+  url "https://github.com/connor-27/homebrew-submodule-kit/releases/download/v1.0.3/fgit-1.0.3.tar.gz"
+  sha256 "30ca8778c13ad44807150ef7401e9b82c5b7749d855538ce9a3f77d52c9ede6a"
   version "1.0.3"
 
   def install
