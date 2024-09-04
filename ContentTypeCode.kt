94fa249637d994f5ea5ca2e8c75f4a9545633363
connor.27 <116634982+connor-27@users.noreply.github.com>
2024-09-04 14:51:57 +0900

    Update ContentTypeCode.kt

---
+++ ContentTypeCode.kt
@@ -0,0 +1,14 @@
+
+package com.kakaoent.fanz.support.database.enum
+
+enum class ContentTypeCode(
+    val projectCode: Int,
+    val projectName: String,
+    val projectServiceCode: Int,
+    val projectServiceName: String
+) {
+    FANZ_108(100, "FANZ", 108, "댓글"),
+    FANZ_103(100, "FANZ", 103, "미디어"),
+    FANZ_102(100, "FANZ", 102, "포스트"),
+    FANZ_101(100, "FANZ", 101, "아티스트"),
+}
